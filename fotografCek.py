21546353836d17a43672f481cc2a96f70b9074f4
orhanxakarsu <56847451+orhanxakarsu@users.noreply.github.com>
2021-10-17 03:48:53 +0300

    Add files via upload

---
+++ fotografCek.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+headers_parameters={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+def resimCek(aranilacakBaslik,turSayisi,dosyaYolu=""):
+    path ="https://www.istockphoto.com/tr/search/2/image?phrase="
+    splitVeri = aranilacakBaslik.split(" ")
+    print(splitVeri)
+    for i in range(0,len(splitVeri)-1):
+        path=path+splitVeri[i]+"%20"
+    path=path+splitVeri[-1]
+    path=path+"&page=1"
+    print(path)
+    butunIdler = []
+    for i in range(1,turSayisi):
+        bulunanYer =path[-1]
+              
+        adres=path[:len(path)-1]       
+        siteAdresi_=adres+str(i)       
+        r = requests.get(siteAdresi_,headers=headers_parameters)
+        soup = BeautifulSoup(r.content, "lxml", from_encoding='UTF-8')       
+        resimId = soup.find_all("img",{"class":"MosiacAsset-module__thumb___L2F4y"})
+        butunIdler.append(resimId)
+           
+    #Cektigimiz resim adreslerini bilgisayara kaydediyoruz :
+    index=0
+    for a,resimAdresi in enumerate(butunIdler):      
+        for j in range(len(resimAdresi)):
+            adres=requests.get(resimAdresi[j]["src"],headers=headers_parameters)
+            file = open(f"{dosyaYolu}/{index}.jpg", "wb")
+            file.write(adres.content)
+            file.close()
+            index+=1   
+    
+def main():
+    resimCek("car",6,"images")
+
+
+if __name__=='__main__':
+    main()
+
